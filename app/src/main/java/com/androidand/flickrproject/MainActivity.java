fd043bf648d7292d126ee1068f3a1b382ccdc173
julien <julien3b@gmail.com>
2016-10-17 11:41:47 +0200

    implement button to change list

--- app/src/main/java/com/androidand/flickrproject/MainActivity.java
+++ app/src/main/java/com/androidand/flickrproject/MainActivity.java
@@ -2,22 +2,39 @@ package com.androidand.flickrproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ArrayList list;
+    FlickrAdapter flick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView listView = (ListView) findViewById(R.id.list);
-        FlickrAdapter flick = new FlickrAdapter(this);
+        flick = new FlickrAdapter(this);
         ArrayList list = new ArrayList<String>();
         list.add("toto");
         list.add("titi");
         flick.setList(list);
         listView.setAdapter(flick);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        list = new ArrayList();
+        list.add("loulou");
+        list.add("lulu");
+        flick.setList(list);
+
+
     }
 }
